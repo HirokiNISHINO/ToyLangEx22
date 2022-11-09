0766438cfe65e3dd6fb297865623641e35211a4a
Hiroki NISHINO <nishino.hiroki@kochi-tech.ac.jp>
2022-11-09 22:10:21 +0900

    fixed a bug in error messages

--- src/kut/compiler/parser/Parser.java
+++ src/kut/compiler/parser/Parser.java
@@ -207,7 +207,7 @@ public class Parser
 		
 		t2 = this.getCurrentToken();
 		if (t2.getC() != ';') {
-			throw new SyntaxErrorException("expected ';' but found: " + t.getL());
+			throw new SyntaxErrorException("expected ';' but found: " + t2);
 		}
 		this.consumeCurrentToken();
 		
@@ -235,7 +235,7 @@ public class Parser
 		
 		t2 = this.getCurrentToken();
 		if (t2.getC() != ';') {
-			throw new SyntaxErrorException("expected ';' but found: " + t.getL());
+			throw new SyntaxErrorException("expected ';' but found: " + t2);
 		}
 		this.consumeCurrentToken();
 
@@ -270,7 +270,7 @@ public class Parser
 		
 		t2 = this.getCurrentToken();
 		if (t2.getC() != ';') {
-			throw new SyntaxErrorException("expected ';' but found: " + t.getL());
+			throw new SyntaxErrorException("expected ';' but found: " + t2);
 		}
 		this.consumeCurrentToken();
 		
@@ -304,7 +304,7 @@ public class Parser
 		
 		t2 = this.getCurrentToken();
 		if (t2.getC() != ';') {
-			throw new SyntaxErrorException("expected ';' but found: " + t.getL());
+			throw new SyntaxErrorException("expected ';' but found: " + t2);
 		}
 		this.consumeCurrentToken();
 		
